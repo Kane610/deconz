027ed2dc938e172e4a0d32a6f00c03422e38b2f5
Robert Svensson <kane610@me.com>
2017-11-05 22:50:08 +0100

    Websocket can now reconnect when Deconz disconnects Cleanup

--- pydeconz/websocket.py
+++ pydeconz/websocket.py
@@ -5,8 +5,17 @@
 import asyncio
 import json
 import logging
+import os
+
+from base64 import encodestring as base64encode
 
 _LOGGER = logging.getLogger(__name__)
+
+STATE_STARTING = 'starting'
+STATE_RUNNING = 'running'
+STATE_STOPPED = 'stopped'
+
+RETRY_TIMER = 15
 
 
 class WSClient(asyncio.Protocol):
@@ -15,26 +24,47 @@ class WSClient(asyncio.Protocol):
     def __init__(self, loop, host, port, callback):
         """Create resources for websocket communication."""
         self.loop = loop
-        self.transport = None
         self.host = host
         self.port = port
         self.callback = callback
-        self.setup_response = True
-        conn = loop.create_connection(lambda: self, host, port)
-        loop.create_task(conn)
+        self.state = None
+        self.transport = None
+
+    def start(self):
+        """Start websocket connection."""
+        conn = self.loop.create_connection(lambda: self, self.host, self.port)
+        task = self.loop.create_task(conn)
+        task.add_done_callback(self.init_done)
+        self.state = STATE_STARTING
+
+    def init_done(self, fut):
+        """Server ready.
+
+        If we get OSError during init the device is not available.
+        """
+        try:
+            if fut.exception():
+                fut.result()
+        except OSError as err:
+            _LOGGER.debug('Got exception %s', err)
+            self.retry()
 
     def stop(self):
         """Close websocket connection."""
+        self.state = STATE_STOPPED
         if self.transport:
             self.transport.close()
+
+    def retry(self):
+        """Retry to connect to Deconz."""
+        self.loop.call_later(RETRY_TIMER, self.start)
+        _LOGGER.debug('Reconnecting to Deconz in %i.', RETRY_TIMER)
 
     def connection_made(self, transport):
         """Do the websocket handshake.
 
         According to https://tools.ietf.org/html/rfc6455
         """
-        import os
-        from base64 import encodestring as base64encode
         randomness = os.urandom(16)
         key = base64encode(randomness).decode('utf-8').strip()
         self.transport = transport
@@ -56,10 +86,11 @@ class WSClient(asyncio.Protocol):
         We need to check how big the header is so we can send event data
         as a proper json object.
         """
-        _LOGGER.debug('Websocket received data: %s', data.decode())
-        if self.setup_response:
-            self.setup_response = False
+        if self.state == STATE_STARTING:
+            self.state = STATE_RUNNING
+            _LOGGER.debug('Websocket handshake: %s', data.decode())
             return
+        _LOGGER.debug('Websocket data: %s', data)
 
         header = ord(data[1:2])
         if header <= 125:
@@ -69,11 +100,12 @@ class WSClient(asyncio.Protocol):
         elif header == 126:
             # Payload information are an extra 2 bytes.
             start = 4
-            end = header + start
+            end = ord(data[3:4]) + start
         elif header == 127:
             # Payload information are an extra 6 bytes.
             start = 8
             end = header + start
+            _LOGGER.error('Websocket received data: %s, %s', data, data[start:end])
 
         event = json.loads(data[start:end].decode())
         self.callback(event)
@@ -81,3 +113,6 @@ class WSClient(asyncio.Protocol):
     def connection_lost(self, exc):
         """Happen when device closes connection or stop() has been called."""
         print('connection_lost', exc)
+        if self.state == STATE_RUNNING:
+            _LOGGER.warning('Lost connection to Deconz')
+            self.retry()
