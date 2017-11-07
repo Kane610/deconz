a45f665d0c1a03eb4347d52ef61fadb8ee76077d
Robert Svensson <kane610@me.com>
2017-11-07 23:29:07 +0100

    Added install script for Deconz

---
+++ install_deconz.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Deconz installer"
+
+cd /tmp/
+if [ -d "deconz" ]; then
+    echo "Removing old Deconz installation"
+    sudo rm -rf deconz
+fi
+mkdir deconz
+cd deconz/
+
+wget http://www.dresden-elektronik.de/rpi/deconz/beta/deconz-2.04.86-qt5.deb
+sudo dpkg --install deconz-2.04.86-qt5.deb
+
+wget http://www.dresden-elektronik.de/rpi/deconz-dev/deconz-dev-2.04.86.deb
+sudo apt-get install --fix-broken --assume-yes
+sudo dpkg --install deconz-dev-2.04.86.deb
+
+git clone https://github.com/dresden-elektronik/deconz-rest-plugin.git
+cd deconz-rest-plugin
+git checkout -b mybranch V2_04_86
+qmake
+make -j3
+cd ..
+sudo cp libde_rest_plugin.so /usr/share/deCONZ/plugins
+
+cd ..
+echo "Cleaning up"
+rm -rf deconz
