d5d0bba3bb2429f991705d223654eb75c4e719c7
Robert Svensson <kane610@me.com>
2017-11-11 23:53:22 +0100

    Bump to v8

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ from setuptools import setup
 setup(
   name='pydeconz',
   packages=['pydeconz'],
-  version='7',
+  version='8',
   description='A python library for communicating with deconz REST-API from dresden elektronik',
   author='Robert Svensson',
   author_email='Kane610@users.noreply.github.com',
   license='MIT',
   url='https://github.com/Kane610/deconz',
-  download_url='https://github.com/Kane610/deconz/archive/v7.tar.gz',
+  download_url='https://github.com/Kane610/deconz/archive/v8.tar.gz',
   install_requires=['aiohttp'],
   keywords=['deconz', 'zigbee', 'homeassistant'],
   classifiers=[
