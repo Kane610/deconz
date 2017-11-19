6336b14ffd9ef461853bbf5e257e9272bac04ce5
Robert Svensson <kane610@me.com>
2017-11-19 18:23:25 +0100

    Bump to v15

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ from setuptools import setup
 setup(
   name='pydeconz',
   packages=['pydeconz'],
-  version='14',
+  version='15',
   description='A python library for communicating with deconz REST-API from dresden elektronik',
   author='Robert Svensson',
   author_email='Kane610@users.noreply.github.com',
   license='MIT',
   url='https://github.com/Kane610/deconz',
-  download_url='https://github.com/Kane610/deconz/archive/v14.tar.gz',
+  download_url='https://github.com/Kane610/deconz/archive/v15.tar.gz',
   install_requires=['aiohttp'],
   keywords=['deconz', 'zigbee', 'homeassistant'],
   classifiers=[
