575e3f58624f5688b8f757295e121f4dddb87497
Robert Svensson <kane610@me.com>
2017-12-26 22:28:04 +0100

    Bump deconz installation to v99

--- install_deconz.sh
+++ install_deconz.sh
@@ -10,16 +10,16 @@ fi
 mkdir deconz
 cd deconz/
 
-wget http://www.dresden-elektronik.de/rpi/deconz/beta/deconz-2.04.86-qt5.deb
-sudo dpkg --install deconz-2.04.86-qt5.deb
+wget http://www.dresden-elektronik.de/rpi/deconz/beta/deconz-2.04.99-qt5.deb
+sudo dpkg --install deconz-2.04.99-qt5.deb
 
-wget http://www.dresden-elektronik.de/rpi/deconz-dev/deconz-dev-2.04.86.deb
+wget http://www.dresden-elektronik.de/rpi/deconz-dev/deconz-dev-2.04.99.deb
 sudo apt-get install --fix-broken --assume-yes
-sudo dpkg --install deconz-dev-2.04.86.deb
+sudo dpkg --install deconz-dev-2.04.99.deb
 
 git clone https://github.com/dresden-elektronik/deconz-rest-plugin.git
 cd deconz-rest-plugin
-git checkout -b mybranch V2_04_86
+git checkout -b mybranch V2_04_99
 qmake
 make -j3
 cd ..
