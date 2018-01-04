3f4d2d0accb4ab386da3000017636b37f207ba49
Robert Svensson <kane610@me.com>
2018-01-05 00:20:57 +0100

    Pylint setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+"""Setup for deCONZ"""
+
 # https://jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
 # http://peterdowns.com/posts/first-time-with-pypi.html
-# pip install /home/pi/pydeconz/ --target /home/pi/home-assistant/venv/lib/python3.4/site-packages/ --upgrade
 # pip install -e /home/pi/pydeconz/
 # Upload to PyPI Live
 # python setup.py sdist bdist_wheel
@@ -9,19 +10,19 @@
 from setuptools import setup
 
 setup(
-  name='pydeconz',
-  packages=['pydeconz'],
-  version='24',
-  description='A Python library for communicating with deCONZ REST-API from Dresden Elektronik',
-  author='Robert Svensson',
-  author_email='Kane610@users.noreply.github.com',
-  license='MIT',
-  url='https://github.com/Kane610/deconz',
-  download_url='https://github.com/Kane610/deconz/archive/v24.tar.gz',
-  install_requires=['aiohttp'],
-  keywords=['deconz', 'zigbee', 'homeassistant'],
-  classifiers=[
-    'Natural Language :: English',
-    'Programming Language :: Python :: 3',
-  ],
+    name='pydeconz',
+    packages=['pydeconz'],
+    version='24',
+    description='A Python library for communicating with deCONZ REST-API from Dresden Elektronik',
+    author='Robert Svensson',
+    author_email='Kane610@users.noreply.github.com',
+    license='MIT',
+    url='https://github.com/Kane610/deconz',
+    download_url='https://github.com/Kane610/deconz/archive/v24.tar.gz',
+    install_requires=['aiohttp'],
+    keywords=['deconz', 'zigbee', 'homeassistant'],
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
 )
