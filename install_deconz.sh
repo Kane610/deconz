814c38e946fa7a565b9b0b660a71f8bdd5be392f
Robert Svensson <kane610@me.com>
2018-02-20 21:57:41 +0100

    Clean up of install script

--- install_deconz.sh
+++ install_deconz.sh
@@ -10,20 +10,11 @@ fi
 mkdir deconz
 cd deconz/
 
-wget http://www.dresden-elektronik.de/rpi/deconz/beta/deconz-2.04.99-qt5.deb
-sudo dpkg --install deconz-2.04.99-qt5.deb
+wget http://www.dresden-elektronik.de/rpi/deconz/beta/deconz-2.05.02-qt5.deb
+sudo dpkg --install deconz-2.05.02-qt5.deb
 
-wget http://www.dresden-elektronik.de/rpi/deconz-dev/deconz-dev-2.04.99.deb
-sudo apt-get install --fix-broken --assume-yes
-sudo dpkg --install deconz-dev-2.04.99.deb
-
-git clone https://github.com/dresden-elektronik/deconz-rest-plugin.git
-cd deconz-rest-plugin
-git checkout -b mybranch V2_04_99
-qmake
-make -j3
-cd ..
-sudo cp libde_rest_plugin.so /usr/share/deCONZ/plugins
+sudo apt-get update
+sudo apt-get install -f
 
 cd ..
 echo "Cleaning up"
