29c09004cf7549e2696bdbfbb3c1cb09cbb8e035
Robert Svensson <kane610@me.com>
2018-08-01 00:16:07 +0200

    Bump to v43 Removed old deCONZ install script

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ from setuptools import setup
 setup(
     name='pydeconz',
     packages=['pydeconz'],
-    version='42',
+    version='43',
     description='A Python library for communicating with deCONZ REST-API from Dresden Elektronik',
     author='Robert Svensson',
     author_email='Kane610@users.noreply.github.com',
     license='MIT',
     url='https://github.com/Kane610/deconz',
-    download_url='https://github.com/Kane610/deconz/archive/v42.tar.gz',
+    download_url='https://github.com/Kane610/deconz/archive/v43.tar.gz',
     install_requires=['aiohttp'],
     keywords=['deconz', 'zigbee', 'homeassistant'],
     classifiers=[
