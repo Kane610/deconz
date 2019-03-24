f19a60f3082e55ceb9c108d29c6f4a7b7e2cf599
Robert Svensson <kane610@me.com>
2019-03-24 17:07:58 +0100

    Fix hound

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -1,13 +1,13 @@
-"""Test pydeCONZ session class.
+"""Test pydeCONZ sensors.
 
-pytest --cov-report term-missing --cov=pydeconz tests/test_init.py
+pytest --cov-report term-missing --cov=pydeconz.sensor tests/test_sensors.py
 """
-import asyncio
-from unittest.mock import Mock, patch
+
+from unittest.mock import Mock
 
 from pydeconz.sensor import (
-    DECONZ_BINARY_SENSOR, DECONZ_SENSOR, OTHER_SENSOR, VIBRATION, create_sensor,
-    supported_sensor)
+    DECONZ_BINARY_SENSOR, DECONZ_SENSOR, OTHER_SENSOR,
+    create_sensor, supported_sensor)
 
 
 async def test_create_sensor():
@@ -50,21 +50,21 @@ async def test_vibration_sensor():
     """Verify that vibration sensor works."""
     sensor = create_sensor('0', FIXTURE_VIBRATION, None)
 
-    assert sensor.state == True
-    assert sensor.is_tripped == True
+    assert sensor.state is True
+    assert sensor.is_tripped is True
     assert sensor.orientation == [10, 1059, 0]
     assert sensor.sensitivity == 21
     assert sensor.sensitivitymax == 21
     assert sensor.tiltangle == 83
-    assert sensor.vibration == True
+    assert sensor.vibration is True
     assert sensor.vibrationstrength == 114
 
     # DeconzSensor
     assert sensor.battery == 91
     assert sensor.ep == 1
-    assert sensor.lowbattery == None
-    assert sensor.on == True
-    assert sensor.reachable == True
+    assert sensor.lowbattery is None
+    assert sensor.on is True
+    assert sensor.reachable is True
 
     # DeconzDevice
     assert sensor.deconz_id == '/sensors/0'
