47fc10318a4a69e7f0ca3ac8fa4fb08b3f17cc8d
Robert Svensson <kane610@me.com>
2019-07-27 10:15:14 +0200

    Bump to v61

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ from setuptools import setup
 setup(
     name='pydeconz',
     packages=['pydeconz'],
-    version='60',
+    version='61',
     description='A Python library for communicating with deCONZ REST-API from Dresden Elektronik',
     author='Robert Svensson',
     author_email='Kane610@users.noreply.github.com',
     license='MIT',
     url='https://github.com/Kane610/deconz',
-    download_url='https://github.com/Kane610/deconz/archive/v60.tar.gz',
+    download_url='https://github.com/Kane610/deconz/archive/v61.tar.gz',
     install_requires=['aiohttp'],
     keywords=['deconz', 'zigbee', 'homeassistant'],
     classifiers=[
