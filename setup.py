5250ba74310cb2da97c20b02b634b74f5d77b362
Robert Svensson <kane610@me.com>
2019-12-08 13:42:25 +0100

    Bump to 65 Run Black on setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,16 @@
 from setuptools import setup
 
 setup(
-    name='pydeconz',
-    packages=['pydeconz'],
-    version='64',
-    description='A Python library for communicating with deCONZ REST-API from Dresden Elektronik',
-    author='Robert Svensson',
-    author_email='Kane610@users.noreply.github.com',
-    license='MIT',
-    url='https://github.com/Kane610/deconz',
-    download_url='https://github.com/Kane610/deconz/archive/v64.tar.gz',
-    install_requires=['aiohttp'],
-    keywords=['deconz', 'zigbee', 'homeassistant'],
-    classifiers=[
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-    ],
+    name="pydeconz",
+    packages=["pydeconz"],
+    version="65",
+    description="A Python library for communicating with deCONZ REST-API from Dresden Elektronik",
+    author="Robert Svensson",
+    author_email="Kane610@users.noreply.github.com",
+    license="MIT",
+    url="https://github.com/Kane610/deconz",
+    download_url="https://github.com/Kane610/deconz/archive/v65.tar.gz",
+    install_requires=["aiohttp"],
+    keywords=["deconz", "zigbee", "homeassistant"],
+    classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
 )
