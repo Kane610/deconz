2a51655128148fe77a838097234877f6df6d2b2a
Robert Svensson <kane610@me.com>
2020-02-02 14:11:40 +0100

    Bump to v70 Specify python requirement to minimum 3.7.0

--- setup.py
+++ setup.py
@@ -12,14 +12,15 @@ from setuptools import setup
 setup(
     name="pydeconz",
     packages=["pydeconz"],
-    version="69",
+    version="70",
     description="A Python library for communicating with deCONZ REST-API from Dresden Elektronik",
     author="Robert Svensson",
     author_email="Kane610@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Kane610/deconz",
-    download_url="https://github.com/Kane610/deconz/archive/v69.tar.gz",
+    download_url="https://github.com/Kane610/deconz/archive/v70.tar.gz",
     install_requires=["aiohttp"],
     keywords=["deconz", "zigbee", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
+    python_requires=">=3.7.0",
 )
