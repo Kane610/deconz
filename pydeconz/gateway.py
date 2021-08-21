09a63d3395641fa457a8e1075a3c86898651fc15
Robert Svensson <kane610@me.com>
2021-08-21 23:45:51 +0200

    Fix isort

--- pydeconz/gateway.py
+++ pydeconz/gateway.py
@@ -7,7 +7,7 @@ from typing import Any, Callable, Dict, Optional, Union
 from aiohttp import client_exceptions
 
 from .alarm_system import RESOURCE_TYPE as ALARM_SYSTEM_RESOURCE, AlarmSystems
-from .config import DeconzConfig, RESOURCE_TYPE as CONFIG_RESOURCE
+from .config import RESOURCE_TYPE as CONFIG_RESOURCE, DeconzConfig
 from .errors import RequestError, ResponseError, raise_error
 from .group import RESOURCE_TYPE as GROUP_RESOURCE, DeconzScene, Groups
 from .light import RESOURCE_TYPE as LIGHT_RESOURCE, Light, Lights
