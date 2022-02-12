68552fe0cdc81d4f76dbdd3d807392969f1d793a
Robert Svensson <Kane610@users.noreply.github.com>
2022-02-12 22:43:09 +0100

    Move groups to interfaces

--- pydeconz/group.py
+++ pydeconz/group.py
@@ -1,24 +1,4 @@
 """Python library to connect deCONZ and Home Assistant to work together."""
 
-from __future__ import annotations
-
-from collections.abc import Awaitable, Callable
-from typing import Any, Final
-
-from .api import APIItems
+from .interfaces.groups import *  # noqa: F401, F403
 from .models.group import *  # noqa: F401, F403
-from .models.group import Group
-
-URL: Final = "/groups"
-
-
-class Groups(APIItems):
-    """Represent deCONZ groups."""
-
-    def __init__(
-        self,
-        raw: dict,
-        request: Callable[..., Awaitable[dict[str, Any]]],
-    ) -> None:
-        """Initialize group manager."""
-        super().__init__(raw, request, URL, Group)
