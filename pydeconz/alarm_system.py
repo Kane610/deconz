7f00c9066d6b4802ff000c583013436365e52b96
Robert Svensson <Kane610@users.noreply.github.com>
2022-02-13 09:07:04 +0100

    Break out scenes from groups (#211)
    * Break out scenes from groups
    
    * Isort imports

--- pydeconz/alarm_system.py
+++ pydeconz/alarm_system.py
@@ -1,4 +1,6 @@
 """Python library to connect deCONZ and Home Assistant to work together."""
 
+# Legacy imports kept for backwards compatibility
+
 from .interfaces.alarm_systems import *  # noqa: F401, F403
 from .models.alarm_system import *  # noqa: F401, F403
--- pydeconz/api.py
+++ pydeconz/api.py
@@ -1,4 +1,6 @@
 """API base classes."""
 
+# Legacy imports kept for backwards compatibility
+
 from .interfaces.api import APIItems  # noqa: F401
 from .models.api import APIItem  # noqa: F401
