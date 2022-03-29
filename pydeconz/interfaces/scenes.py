19a2ce49a4b0cb889f39d1e3aa58d7335c56f2e1
Robert Svensson <Kane610@users.noreply.github.com>
2022-03-29 20:50:35 +0200

    Bring resource control into scene interface

--- pydeconz/interfaces/scenes.py
+++ pydeconz/interfaces/scenes.py
@@ -34,6 +34,46 @@ class Scenes(APIItems[Scene]):
             json={"name": name},
         )
 
+    async def recall(self, group_id: str, scene_id: str) -> dict[str, Any]:
+        """Recall scene to group."""
+        return await self.gateway.request(
+            "put",
+            path=f"/groups/{group_id}/scenes/{scene_id}/recall",
+            json={},
+        )
+
+    async def store(self, group_id: str, scene_id: str) -> dict[str, Any]:
+        """Store current group state in scene.
+
+        The actual state of each light in the group will become the lights scene state.
+        """
+        return await self.gateway.request(
+            "put",
+            path=f"/groups/{group_id}/scenes/{scene_id}/store",
+            json={},
+        )
+
+    async def set_attributes(
+        self, group_id: str, scene_id: str, name: str | None = None
+    ) -> dict[str, Any]:
+        """Change attributes of scene.
+
+        Supported values:
+        - name [str]
+        """
+        data = {
+            key: value
+            for key, value in {
+                "name": name,
+            }.items()
+            if value is not None
+        }
+        return await self.gateway.request(
+            "put",
+            path=f"/groups/{group_id}/scenes/{scene_id}",
+            json=data,
+        )
+
     def group_data_callback(self, action: EventType, group_id: str) -> None:
         """Subscribe callback for new group data."""
         self.process_item(group_id, {})
