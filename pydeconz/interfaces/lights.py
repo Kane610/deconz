cae625a7c22e2c07b23af7fb393203045d6f1f21
Robert Svensson <Kane610@users.noreply.github.com>
2022-03-30 21:44:33 +0200

    Add resource controls to lights (#229)
    * Add controls to covers
    
    * Add controls to fans
    
    * Add controls to locks
    
    * Add controls to siren
    
    * Add resource control to lights

--- pydeconz/interfaces/lights.py
+++ pydeconz/interfaces/lights.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import enum
 from typing import TYPE_CHECKING, Any, Union
 
 from ..models import ResourceGroup, ResourceType
@@ -17,6 +18,50 @@ from .api import APIItems, GroupedAPIItems
 
 if TYPE_CHECKING:
     from ..gateway import DeconzSession
+
+
+class Alert(enum.Enum):
+    """Temporary alert effect.
+
+    "none" — light is not performing an alert.
+    "lselect" — light is blinking a longer time.
+    "select" — light is blinking a short time.
+    """
+
+    NONE = "none"
+    LONG = "lselect"
+    SHORT = "select"
+
+
+class CoverAction(enum.Enum):
+    """Possible cover actions."""
+
+    CLOSE = enum.auto()
+    OPEN = enum.auto()
+    STOP = enum.auto()
+
+
+class Effect(enum.Enum):
+    """Effect of the light.
+
+    "colorloop" — cycle through hue values 0–360.
+    "none" — no effect.
+    """
+
+    COLORLOOP = "colorloop"
+    NONE = "none"
+
+
+class FanSpeed(enum.Enum):
+    """Possible fan speeds."""
+
+    OFF = 0
+    PERCENT_25 = 1
+    PERCENT_50 = 2
+    PERCENT_75 = 3
+    PERCENT_100 = 4
+    AUTO = 5
+    COMFORT_BREEZE = 6
 
 
 class ConfigurationToolHandler(APIItems[ConfigurationTool]):
@@ -38,6 +83,40 @@ class CoverHandler(APIItems[Cover]):
     }
     item_cls = Cover
 
+    async def set_state(
+        self,
+        id: str,
+        action: CoverAction | None = None,
+        lift: int | None = None,
+        tilt: int | None = None,
+    ) -> dict[str, Any]:
+        """Set state of cover.
+
+        Action [CoverAction] Open, Close, Stop
+        Lift [int] between 0-100.
+        Tilt [int] between 0-100.
+        """
+        data: dict[str, bool | int] = {}
+
+        if action is not None:
+            if action is CoverAction.OPEN:
+                data["open"] = True
+            elif action is CoverAction.CLOSE:
+                data["open"] = False
+            elif action is CoverAction.STOP:
+                data["stop"] = True
+        else:
+            if lift is not None:
+                data["lift"] = lift
+            if tilt is not None:
+                data["tilt"] = tilt
+
+        return await self.gateway.request(
+            "put",
+            path=f"{self.path}/{id}/state",
+            json=data,
+        )
+
 
 class FanHandler(APIItems[Fan]):
     """Handler for locks."""
@@ -45,6 +124,17 @@ class FanHandler(APIItems[Fan]):
     resource_group = ResourceGroup.LIGHT
     resource_type = ResourceType.FAN
     item_cls = Fan
+
+    async def set_speed(self, id: str, speed: FanSpeed) -> dict[str, Any]:
+        """Set speed of fans/ventilators.
+
+        Speed [FanSpeed] Off, 25%, 50%, 75%, 100%, Auto, ComfortBreeze.
+        """
+        return await self.gateway.request(
+            "put",
+            path=f"{self.path}/{id}/state",
+            json={"speed": speed.value},
+        )
 
 
 class LightHandler(APIItems[Light]):
@@ -66,6 +156,70 @@ class LightHandler(APIItems[Light]):
     }
     item_cls = Light
 
+    async def set_state(
+        self,
+        id: str,
+        alert: Alert | None = None,
+        brightness: int | None = None,
+        color_loop_speed: int | None = None,
+        color_temperature: int | None = None,
+        effect: Effect | None = None,
+        hue: int | None = None,
+        on: bool | None = None,
+        on_time: int | None = None,
+        saturation: int | None = None,
+        transition_time: int | None = None,
+        xy: tuple[float, float] | None = None,
+    ) -> dict[str, Any]:
+        """Change state of a light.
+
+        Supported values:
+        - alert [str]
+          - "none" light is not performing an alert
+          - "select" light is blinking a short time
+          - "lselect" light is blinking a longer time
+        - brightness [int] 0-255
+        - color_loop_speed [int] 1-255
+          - 1 = very fast
+          - 15 is default
+          - 255 very slow
+        - color_temperature [int] between ctmin-ctmax
+        - effect [str]
+          - "none" no effect
+          - "colorloop" the light will cycle continuously through all
+                        colors with the speed specified by colorloopspeed
+        - hue [int] 0-65535
+        - on [bool] True/False
+        - on_time [int] 0-65535 1/10 seconds resolution
+        - saturation [int] 0-255
+        - transition_time [int] 0-65535 1/10 seconds resolution
+        - xy [tuple] (0-1, 0-1)
+        """
+        data: dict[str, Any] = {
+            key: value
+            for key, value in {
+                "bri": brightness,
+                "colorloopspeed": color_loop_speed,
+                "ct": color_temperature,
+                "hue": hue,
+                "on": on,
+                "ontime": on_time,
+                "sat": saturation,
+                "transitiontime": transition_time,
+                "xy": xy,
+            }.items()
+            if value is not None
+        }
+        if alert is not None:
+            data["alert"] = alert.value
+        if effect is not None:
+            data["effect"] = effect.value
+        return await self.gateway.request(
+            "put",
+            path=f"{self.path}/{id}/state",
+            json=data,
+        )
+
 
 class LockHandler(APIItems[Lock]):
     """Handler for fans."""
@@ -74,6 +228,17 @@ class LockHandler(APIItems[Lock]):
     resource_type = ResourceType.DOOR_LOCK
     item_cls = Lock
 
+    async def set_state(self, id: str, lock: bool) -> dict[str, Any]:
+        """Set state of lock.
+
+        Lock [bool] True/False.
+        """
+        return await self.gateway.request(
+            "put",
+            path=f"{self.path}/{id}/state",
+            json={"on": lock},
+        )
+
 
 class SirenHandler(APIItems[Siren]):
     """Handler for sirens."""
@@ -81,6 +246,26 @@ class SirenHandler(APIItems[Siren]):
     resource_group = ResourceGroup.LIGHT
     resource_type = ResourceType.WARNING_DEVICE
     item_cls = Siren
+
+    async def set_state(
+        self, id: str, on: bool, duration: int | None = None
+    ) -> dict[str, Any]:
+        """Turn on device.
+
+        Duration is counted as 1/10 of a second.
+        """
+        data: dict[str, int | str] = {}
+        if on:
+            data["alert"] = "lselect"
+            if duration is not None:
+                data["ontime"] = duration
+        else:
+            data["alert"] = "none"
+        return await self.gateway.request(
+            "put",
+            path=f"{self.path}/{id}/state",
+            json=data,
+        )
 
 
 LightResources = Union[
