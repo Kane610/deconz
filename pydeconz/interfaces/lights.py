266e190d078d3b0df84311bc504bab73dc164755
Robert Svensson <Kane610@users.noreply.github.com>
2022-03-30 21:33:18 +0200

    Add resource control to lights

--- pydeconz/interfaces/lights.py
+++ pydeconz/interfaces/lights.py
@@ -20,12 +20,36 @@ if TYPE_CHECKING:
     from ..gateway import DeconzSession
 
 
+class Alert(enum.Enum):
+    """Temporary alert effect.
+
+    "none" — light is not performing an alert.
+    "lselect" — light is blinking a longer time.
+    "select" — light is blinking a short time.
+    """
+
+    NONE = "none"
+    LONG = "lselect"
+    SHORT = "select"
+
+
 class CoverAction(enum.Enum):
     """Possible cover actions."""
 
     CLOSE = enum.auto()
     OPEN = enum.auto()
     STOP = enum.auto()
+
+
+class Effect(enum.Enum):
+    """Effect of the light.
+
+    "colorloop" — cycle through hue values 0–360.
+    "none" — no effect.
+    """
+
+    COLORLOOP = "colorloop"
+    NONE = "none"
 
 
 class FanSpeed(enum.Enum):
@@ -131,6 +155,70 @@ class LightHandler(APIItems[Light]):
         ResourceType.UNKNOWN,  # Legacy support
     }
     item_cls = Light
+
+    async def set_state(
+        self,
+        id: str,
+        alert: Alert | None = None,
+        brightness: int | None = None,
+        color_loop_speed: int | None = None,
+        color_temperature: int | None = None,
+        effect: Effect | None = None,
+        hue: int | None = None,
+        on: bool | None = None,
+        on_time: int | None = None,
+        saturation: int | None = None,
+        transition_time: int | None = None,
+        xy: tuple[float, float] | None = None,
+    ) -> dict[str, Any]:
+        """Change state of a light.
+
+        Supported values:
+        - alert [str]
+          - "none" light is not performing an alert
+          - "select" light is blinking a short time
+          - "lselect" light is blinking a longer time
+        - brightness [int] 0-255
+        - color_loop_speed [int] 1-255
+          - 1 = very fast
+          - 15 is default
+          - 255 very slow
+        - color_temperature [int] between ctmin-ctmax
+        - effect [str]
+          - "none" no effect
+          - "colorloop" the light will cycle continuously through all
+                        colors with the speed specified by colorloopspeed
+        - hue [int] 0-65535
+        - on [bool] True/False
+        - on_time [int] 0-65535 1/10 seconds resolution
+        - saturation [int] 0-255
+        - transition_time [int] 0-65535 1/10 seconds resolution
+        - xy [tuple] (0-1, 0-1)
+        """
+        data: dict[str, Any] = {
+            key: value
+            for key, value in {
+                "bri": brightness,
+                "colorloopspeed": color_loop_speed,
+                "ct": color_temperature,
+                "hue": hue,
+                "on": on,
+                "ontime": on_time,
+                "sat": saturation,
+                "transitiontime": transition_time,
+                "xy": xy,
+            }.items()
+            if value is not None
+        }
+        if alert is not None:
+            data["alert"] = alert.value
+        if effect is not None:
+            data["effect"] = effect.value
+        return await self.gateway.request(
+            "put",
+            path=f"{self.path}/{id}/state",
+            json=data,
+        )
 
 
 class LockHandler(APIItems[Lock]):
--- tests/test_lights.py
+++ tests/test_lights.py
@@ -7,7 +7,7 @@ from unittest.mock import Mock
 
 import pytest
 
-from pydeconz.interfaces.lights import CoverAction, FanSpeed
+from pydeconz.interfaces.lights import Alert, CoverAction, Effect, FanSpeed
 from pydeconz.models.light.fan import FAN_SPEED_100_PERCENT
 from pydeconz.models.light import (
     ALERT_KEY,
@@ -29,146 +29,6 @@ def deconz_light(deconz_refresh_state):
         return deconz_session.lights["0"]
 
     yield data_to_deconz_session
-
-
-async def test_create_light(mock_aioresponse, deconz_light, deconz_called_with):
-    """Verify that creating a light works."""
-    light = await deconz_light(
-        {
-            "ctmax": 500,
-            "ctmin": 153,
-            "etag": "026bcfe544ad76c7534e5ca8ed39047c",
-            "hascolor": True,
-            "manufacturername": "dresden elektronik",
-            "modelid": "FLS-PP3",
-            "name": "Light 1",
-            "pointsymbol": {},
-            "state": {
-                "alert": None,
-                "bri": 111,
-                "colormode": "ct",
-                "ct": 307,
-                "effect": None,
-                "hascolor": True,
-                "hue": 7998,
-                "on": False,
-                "reachable": True,
-                "sat": 172,
-                "xy": [0.421253, 0.39921],
-            },
-            "swversion": "020C.201000A0",
-            "type": "Extended color light",
-            "uniqueid": "00:21:2E:FF:FF:00:73:9F-0A",
-        }
-    )
-
-    assert light.state is False
-    assert light.on is False
-    assert light.alert is None
-
-    assert light.brightness == 111
-    assert light.hue == 7998
-    assert light.saturation == 172
-    assert light.color_temp == 307
-    assert light.xy == (0.421253, 0.39921)
-    assert light.color_mode == "ct"
-    assert light.max_color_temp == 500
-    assert light.min_color_temp == 153
-    assert light.effect is None
-    assert light.reachable is True
-
-    assert light.deconz_id == "/lights/0"
-    assert light.etag == "026bcfe544ad76c7534e5ca8ed39047c"
-    assert light.manufacturer == "dresden elektronik"
-    assert light.model_id == "FLS-PP3"
-    assert light.name == "Light 1"
-    assert light.software_version == "020C.201000A0"
-    assert light.type == "Extended color light"
-    assert light.unique_id == "00:21:2E:FF:FF:00:73:9F-0A"
-
-    mock_callback = Mock()
-    light.register_callback(mock_callback)
-    assert light._callbacks
-
-    event = {"state": {"xy": [0.1, 0.1]}}
-    light.update(event)
-
-    assert light.brightness == 111
-    assert light.xy == (0.1, 0.1)
-    mock_callback.assert_called_once()
-    assert light.changed_keys == {"state", "xy"}
-
-    light.remove_callback(mock_callback)
-    assert not light._callbacks
-
-    light.raw["state"]["xy"] = (65555, 65555)
-    assert light.xy == (1, 1)
-
-    del light.raw["state"]["xy"]
-    assert light.xy is None
-
-    light.raw["ctmax"] = 1000
-    assert light.max_color_temp == 650
-
-    light.raw["ctmin"] = 0
-    assert light.min_color_temp == 140
-
-    del light.raw["ctmax"]
-    assert light.max_color_temp is None
-
-    del light.raw["ctmin"]
-    assert light.min_color_temp is None
-
-    mock_aioresponse.put("http://host:80/api/apikey/lights/0")
-    await light.set_attributes(name="light")
-    assert deconz_called_with(
-        "put",
-        path="/lights/0",
-        json={"name": "light"},
-    )
-
-    mock_aioresponse.put("http://host:80/api/apikey/lights/0/state")
-    await light.set_state(
-        alert=ALERT_SHORT,
-        brightness=200,
-        color_loop_speed=10,
-        color_temperature=400,
-        effect=EFFECT_COLOR_LOOP,
-        hue=1000,
-        on=True,
-        on_time=100,
-        saturation=150,
-        transition_time=250,
-        xy=(0.1, 0.1),
-    )
-    assert deconz_called_with(
-        "put",
-        path="/lights/0/state",
-        json={
-            "alert": "select",
-            "bri": 200,
-            "colorloopspeed": 10,
-            "ct": 400,
-            "effect": "colorloop",
-            "hue": 1000,
-            "on": True,
-            "ontime": 100,
-            "sat": 150,
-            "transitiontime": 250,
-            "xy": (0.1, 0.1),
-        },
-    )
-
-    mock_aioresponse.put("http://host:80/api/apikey/lights/0/state")
-    await light.set_state(on=False)
-    assert deconz_called_with(
-        "put",
-        path="/lights/0/state",
-        json={"on": False},
-    )
-
-    light.update({"state": {"bri": 2}})
-    assert light.brightness == 2
 
 
 async def test_configuration_tool(deconz_light):
@@ -516,6 +376,192 @@ async def test_create_fan(mock_aioresponse, deconz_light, deconz_called_with):
 
     fan.remove_callback(mock_callback)
     assert not fan._callbacks
+
+
+async def test_control_light(mock_aioresponse, deconz_session, deconz_called_with):
+    """Verify that creating a light works."""
+    lights = deconz_session.lights.lights
+
+    mock_aioresponse.put("http://host:80/api/apikey/lights/0/state")
+    await lights.set_state(
+        id="0",
+        alert=Alert.SHORT,
+        brightness=200,
+        color_loop_speed=10,
+        color_temperature=400,
+        effect=Effect.COLORLOOP,
+        hue=1000,
+        on=True,
+        on_time=100,
+        saturation=150,
+        transition_time=250,
+        xy=(0.1, 0.1),
+    )
+    assert deconz_called_with(
+        "put",
+        path="/lights/0/state",
+        json={
+            "alert": "select",
+            "bri": 200,
+            "colorloopspeed": 10,
+            "ct": 400,
+            "effect": "colorloop",
+            "hue": 1000,
+            "on": True,
+            "ontime": 100,
+            "sat": 150,
+            "transitiontime": 250,
+            "xy": (0.1, 0.1),
+        },
+    )
+
+    mock_aioresponse.put("http://host:80/api/apikey/lights/0/state")
+    await lights.set_state("0", on=False)
+    assert deconz_called_with(
+        "put",
+        path="/lights/0/state",
+        json={"on": False},
+    )
+
+
+async def test_create_light(mock_aioresponse, deconz_light, deconz_called_with):
+    """Verify that creating a light works."""
+    light = await deconz_light(
+        {
+            "ctmax": 500,
+            "ctmin": 153,
+            "etag": "026bcfe544ad76c7534e5ca8ed39047c",
+            "hascolor": True,
+            "manufacturername": "dresden elektronik",
+            "modelid": "FLS-PP3",
+            "name": "Light 1",
+            "pointsymbol": {},
+            "state": {
+                "alert": None,
+                "bri": 111,
+                "colormode": "ct",
+                "ct": 307,
+                "effect": None,
+                "hascolor": True,
+                "hue": 7998,
+                "on": False,
+                "reachable": True,
+                "sat": 172,
+                "xy": [0.421253, 0.39921],
+            },
+            "swversion": "020C.201000A0",
+            "type": "Extended color light",
+            "uniqueid": "00:21:2E:FF:FF:00:73:9F-0A",
+        }
+    )
+
+    assert light.state is False
+    assert light.on is False
+    assert light.alert is None
+
+    assert light.brightness == 111
+    assert light.hue == 7998
+    assert light.saturation == 172
+    assert light.color_temp == 307
+    assert light.xy == (0.421253, 0.39921)
+    assert light.color_mode == "ct"
+    assert light.max_color_temp == 500
+    assert light.min_color_temp == 153
+    assert light.effect is None
+    assert light.reachable is True
+
+    assert light.deconz_id == "/lights/0"
+    assert light.etag == "026bcfe544ad76c7534e5ca8ed39047c"
+    assert light.manufacturer == "dresden elektronik"
+    assert light.model_id == "FLS-PP3"
+    assert light.name == "Light 1"
+    assert light.software_version == "020C.201000A0"
+    assert light.type == "Extended color light"
+    assert light.unique_id == "00:21:2E:FF:FF:00:73:9F-0A"
+
+    mock_callback = Mock()
+    light.register_callback(mock_callback)
+    assert light._callbacks
+
+    event = {"state": {"xy": [0.1, 0.1]}}
+    light.update(event)
+
+    assert light.brightness == 111
+    assert light.xy == (0.1, 0.1)
+    mock_callback.assert_called_once()
+    assert light.changed_keys == {"state", "xy"}
+
+    light.remove_callback(mock_callback)
+    assert not light._callbacks
+
+    light.raw["state"]["xy"] = (65555, 65555)
+    assert light.xy == (1, 1)
+
+    del light.raw["state"]["xy"]
+    assert light.xy is None
+
+    light.raw["ctmax"] = 1000
+    assert light.max_color_temp == 650
+
+    light.raw["ctmin"] = 0
+    assert light.min_color_temp == 140
+
+    del light.raw["ctmax"]
+    assert light.max_color_temp is None
+
+    del light.raw["ctmin"]
+    assert light.min_color_temp is None
+
+    mock_aioresponse.put("http://host:80/api/apikey/lights/0")
+    await light.set_attributes(name="light")
+    assert deconz_called_with(
+        "put",
+        path="/lights/0",
+        json={"name": "light"},
+    )
+
+    mock_aioresponse.put("http://host:80/api/apikey/lights/0/state")
+    await light.set_state(
+        alert=ALERT_SHORT,
+        brightness=200,
+        color_loop_speed=10,
+        color_temperature=400,
+        effect=EFFECT_COLOR_LOOP,
+        hue=1000,
+        on=True,
+        on_time=100,
+        saturation=150,
+        transition_time=250,
+        xy=(0.1, 0.1),
+    )
+    assert deconz_called_with(
+        "put",
+        path="/lights/0/state",
+        json={
+            "alert": "select",
+            "bri": 200,
+            "colorloopspeed": 10,
+            "ct": 400,
+            "effect": "colorloop",
+            "hue": 1000,
+            "on": True,
+            "ontime": 100,
+            "sat": 150,
+            "transitiontime": 250,
+            "xy": (0.1, 0.1),
+        },
+    )
+
+    mock_aioresponse.put("http://host:80/api/apikey/lights/0/state")
+    await light.set_state(on=False)
+    assert deconz_called_with(
+        "put",
+        path="/lights/0/state",
+        json={"on": False},
+    )
+
+    light.update({"state": {"bri": 2}})
+    assert light.brightness == 2
 
 
 async def test_control_lock(mock_aioresponse, deconz_session, deconz_called_with):
