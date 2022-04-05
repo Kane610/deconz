b6fce34a0feb9e79f3e263a99524be7d31068b09
Robert Svensson <Kane610@users.noreply.github.com>
2022-04-05 20:47:52 +0200

    Bump to v88

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 MIN_PY_VERSION = "3.9"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 REQUIREMENTS = list(val.strip() for val in open("requirements.txt"))
-VERSION = "87"
+VERSION = "88"
 
 setup(
     name="pydeconz",
@@ -16,7 +16,7 @@ setup(
     author_email="Kane610@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Kane610/deconz",
-    download_url=f"https://github.com/Kane610/deconz/archive/{VERSION}.tar.gz",
+    download_url=f"https://github.com/Kane610/deconz/archive/v{VERSION}.tar.gz",
     install_requires=REQUIREMENTS,
     tests_require=["pytest-aiohttp", "pytest", "aioresponses"],
     keywords=["deconz", "zigbee", "homeassistant"],
