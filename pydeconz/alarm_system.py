679ae42564fd3ecac717347abd2770245417bd78
Robert Svensson <Kane610@users.noreply.github.com>
2022-04-22 23:28:26 +0200

    Remove old alarm_system file

--- pydeconz/alarm_system.py
+++
@@ -1,6 +0,0 @@
-"""Python library to connect deCONZ and Home Assistant to work together."""
-
-# Legacy imports kept for backwards compatibility
-
-from .interfaces.alarm_systems import *  # noqa: F401, F403
-from .models.alarm_system import *  # noqa: F401, F403
