a361dda9481473a9cbb4f1c010db7f91df236f35
Robert Svensson <Kane610@users.noreply.github.com>
2022-04-23 10:06:26 +0200

    Remove legacy filr to support imports of light

--- pydeconz/light.py
+++
@@ -1,6 +0,0 @@
-"""Python library to connect deCONZ and Home Assistant to work together."""
-
-# Legacy imports kept for backwards compatibility
-
-from .interfaces.lights import *  # noqa: F401, F403
-from .models.light import *  # noqa: F401, F403
