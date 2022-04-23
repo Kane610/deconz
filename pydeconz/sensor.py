60c580f04618fdea71c5a5630e56d6dfde5ffb0d
Robert Svensson <Kane610@users.noreply.github.com>
2022-04-23 11:46:13 +0200

    Remove legacy imports of sensors

--- pydeconz/sensor.py
+++
@@ -1,6 +0,0 @@
-"""Python library to connect deCONZ and Home Assistant to work together."""
-
-# Legacy imports kept for backwards compatibility
-
-from .interfaces.sensors import *  # noqa: F401, F403
-from .models.sensor import *  # noqa: F401, F403
