db7a3b003651dfcde29bec3ac570d37d002f8144
Robert Svensson <Kane610@users.noreply.github.com>
2022-04-23 09:16:55 +0200

    Remove unnecessary imports from group and scene

--- pydeconz/group.py
+++
@@ -1,6 +0,0 @@
-"""Python library to connect deCONZ and Home Assistant to work together."""
-
-# Legacy imports kept for backwards compatibility
-
-from .interfaces.groups import *  # noqa: F401, F403
-from .models.group import *  # noqa: F401, F403
