8e14f9e6387eca7f9ecc00823c6a57b26e201531
Robert Svensson <Kane610@users.noreply.github.com>
2022-04-30 18:06:53 +0200

    Explicitly set versions on all packages in requirements/-test/-dev (#254)
    Don't read required packages from requirements.txt

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.9"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-REQUIREMENTS = list(val.strip() for val in open("requirements.txt"))
 VERSION = "91"
 
 setup(
@@ -18,7 +17,7 @@ setup(
     license="MIT",
     url="https://github.com/Kane610/deconz",
     download_url=f"https://github.com/Kane610/deconz/archive/v{VERSION}.tar.gz",
-    install_requires=REQUIREMENTS,
+    install_requires=["aiohttp"],
     tests_require=["pytest-aiohttp", "pytest", "aioresponses"],
     keywords=["deconz", "zigbee", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
