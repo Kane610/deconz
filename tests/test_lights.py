df44441a84b3917da16de4ce295b7027afa41ce8
Robert Svensson <Kane610@users.noreply.github.com>
2022-06-21 22:04:26 +0200

    Break out range extender tests (#283)

--- tests/test_lights.py
+++ tests/test_lights.py
@@ -367,35 +367,6 @@ async def test_create_fan(mock_aioresponse, deconz_light, deconz_called_with):
     assert not fan._callbacks
 
 
-async def test_range_extender(deconz_light):
-    """Verify that range extender work."""
-    range_extender = await deconz_light(
-        {
-            "etag": "62a220a6141a5956a6916633cad0d56f",
-            "hascolor": False,
-            "manufacturername": "IKEA of Sweden",
-            "modelid": "TRADFRI signal repeater",
-            "name": "Range extender 64",
-            "state": {"alert": "none", "reachable": True},
-            "swversion": "2.0.019",
-            "type": "Range extender",
-            "uniqueid": "00:0d:6f:ff:fe:9f:7f:52-01",
-        }
-    )
-
-    assert range_extender.state is None
-    assert range_extender.reachable is True
-
-    assert range_extender.deconz_id == "/lights/0"
-    assert range_extender.etag == "62a220a6141a5956a6916633cad0d56f"
-    assert range_extender.manufacturer == "IKEA of Sweden"
-    assert range_extender.model_id == "TRADFRI signal repeater"
-    assert range_extender.name == "Range extender 64"
-    assert range_extender.software_version == "2.0.019"
-    assert range_extender.type == "Range extender"
-    assert range_extender.unique_id == "00:0d:6f:ff:fe:9f:7f:52-01"
-
-
 async def test_create_all_light_types(deconz_refresh_state):
     """Verify that light types work."""
     deconz_session = await deconz_refresh_state(
