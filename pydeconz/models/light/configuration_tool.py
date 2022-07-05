b37f727012b57923539b2017da83d636f25f9a22
Robert Svensson <Kane610@users.noreply.github.com>
2022-07-05 08:56:55 +0200

    Use ResourceType enum values on lights (#302)

--- pydeconz/models/light/configuration_tool.py
+++ pydeconz/models/light/configuration_tool.py
@@ -1,9 +1,11 @@
 """Python library to connect deCONZ and Home Assistant to work together."""
 
+from pydeconz.models import ResourceType
+
 from . import LightBase
 
 
 class ConfigurationTool(LightBase):
     """deCONZ hardware antenna."""
 
-    ZHATYPE = ("Configuration tool",)
+    ZHATYPE = (ResourceType.CONFIGURATION_TOOL.value,)
--- pydeconz/models/light/range_extender.py
+++ pydeconz/models/light/range_extender.py
@@ -1,9 +1,11 @@
 """Python library to connect deCONZ and Home Assistant to work together."""
 
+from pydeconz.models import ResourceType
+
 from . import LightBase
 
 
 class RangeExtender(LightBase):
     """ZigBee range extender."""
 
-    ZHATYPE = ("Range extender",)
+    ZHATYPE = (ResourceType.RANGE_EXTENDER.value,)
