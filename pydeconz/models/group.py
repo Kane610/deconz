2152ffa1597394a74ddd09bec9ede1c6db47286b
Robert Svensson <Kane610@users.noreply.github.com>
2022-07-15 12:53:05 +0200

    Remove controls from group and light models (#316)

--- pydeconz/models/group.py
+++ pydeconz/models/group.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Any, Final, Literal, TypedDict
+from typing import Final, Literal, TypedDict
 
 from . import ResourceGroup
 from .deconz_device import DeconzDevice
@@ -189,97 +189,6 @@ class Group(DeconzDevice):
         Subsequent ids from multidevices with multiple endpoints.
         """
         return self.raw.get("multideviceids")
-
-    async def set_attributes(
-        self,
-        hidden: bool | None = None,
-        light_sequence: list[str] | None = None,
-        lights: list[str] | None = None,
-        multi_device_ids: list[str] | None = None,
-        name: str | None = None,
-    ) -> dict[str, Any]:
-        """Change attributes of a group.
-
-        Supported values:
-        - hidden [bool] Indicates the hidden status of the group
-        - light_sequence [list of light IDs] Specify a sorted list of light IDs for apps
-        - lights [list of light IDs]IDs of the lights which are members of the group
-        - multi_device_ids [int] Subsequential light IDs of multidevices
-        - name [str] The name of the group
-        """
-        data = {
-            key: value
-            for key, value in {
-                "hidden": hidden,
-                "lightsequence": light_sequence,
-                "lights": lights,
-                "multideviceids": multi_device_ids,
-                "name": name,
-            }.items()
-            if value is not None
-        }
-        return await self.request(field=f"{self.deconz_id}", data=data)
-
-    async def set_state(
-        self,
-        alert: Literal["none", "select", "lselect"] | str | None = None,
-        brightness: int | None = None,
-        color_loop_speed: int | None = None,
-        color_temperature: int | None = None,
-        effect: Literal["colorloop", "none"] | str | None = None,
-        hue: int | None = None,
-        on: bool | None = None,
-        on_time: int | None = None,
-        saturation: int | None = None,
-        toggle: bool | None = None,
-        transition_time: int | None = None,
-        xy: tuple[float, float] | None = None,
-    ) -> dict[str, Any]:
-        """Change state of a group.
-
-        Supported values:
-        - alert [str]
-          - "none" light is not performing an alert
-          - "select" light is blinking a short time
-          - "lselect" light is blinking a longer time
-        - brightness [int] 0-255
-        - color_loop_speed [int] 1-255
-          - 1 = very fast
-          - 15 is default
-          - 255 very slow
-        - color_temperature [int] between ctmin-ctmax
-        - effect [str]
-          - "none" no effect
-          - "colorloop" the light will cycle continuously through all
-                        colors with the speed specified by colorloopspeed
-        - hue [int] 0-65535
-        - on [bool] True/False
-        - on_time [int] 0-65535 1/10 seconds resolution
-        - saturation [int] 0-255
-        - toggle [bool] True toggles the lights of that group from on to off
-                        or vice versa, false has no effect
-        - transition_time [int] 0-65535 1/10 seconds resolution
-        - xy [tuple] 0-1
-        """
-        data = {
-            key: value
-            for key, value in {
-                "alert": alert,
-                "bri": brightness,
-                "colorloopspeed": color_loop_speed,
-                "ct": color_temperature,
-                "effect": effect,
-                "hue": hue,
-                "on": on,
-                "ontime": on_time,
-                "sat": saturation,
-                "toggle": toggle,
-                "transitiontime": transition_time,
-                "xy": xy,
-            }.items()
-            if value is not None
-        }
-        return await self.request(field=f"{self.deconz_id}/action", data=data)
 
     def update_color_state(
         self, light: Light, update_all_attributes: bool = False
