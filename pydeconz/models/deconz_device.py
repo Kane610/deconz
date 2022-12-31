20e362d3af43c9b528f83935e6de50af25fb36f4
Robert Svensson <Kane610@users.noreply.github.com>
2022-12-31 11:11:09 +0100

    Make sure deconz_device properties always return a string

--- pydeconz/models/deconz_device.py
+++ pydeconz/models/deconz_device.py
@@ -14,40 +14,40 @@ class DeconzDevice(APIItem):
     def etag(self) -> str:
         """HTTP etag change on any action to the device."""
         raw: dict[str, str] = self.raw
-        return raw.get("etag", "")
+        return raw.get("etag") or ""
 
     @property
     def manufacturer(self) -> str:
         """Device manufacturer."""
         raw: dict[str, str] = self.raw
-        return raw.get("manufacturername", "")
+        return raw.get("manufacturername") or ""
 
     @property
     def model_id(self) -> str:
         """Device model."""
         raw: dict[str, str] = self.raw
-        return raw.get("modelid", "")
+        return raw.get("modelid") or ""
 
     @property
     def name(self) -> str:
         """Name of the device."""
         raw: dict[str, str] = self.raw
-        return raw.get("name", "")
+        return raw.get("name") or ""
 
     @property
     def software_version(self) -> str:
         """Firmware version."""
         raw: dict[str, str] = self.raw
-        return raw.get("swversion", "")
+        return raw.get("swversion") or ""
 
     @property
     def type(self) -> str:
         """Human readable type of the device."""
         raw: dict[str, str] = self.raw
-        return raw.get("type", "")
+        return raw.get("type") or ""
 
     @property
     def unique_id(self) -> str:
         """Id for unique device identification."""
         raw: dict[str, str] = self.raw
-        return raw.get("uniqueid", "")
+        return raw.get("uniqueid") or ""
