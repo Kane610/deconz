f9bca6db80fb5e19871bbea1f210cfd70a556188
Markus <m4k.join@outlook.com>
2023-02-10 22:58:02 +0100

    add "supports airquality" check

--- pydeconz/models/sensor/air_quality.py
+++ pydeconz/models/sensor/air_quality.py
@@ -87,3 +87,8 @@ class AirQuality(SensorBase):
     def supports_pm_2_5(self) -> bool:
         """Support Air quality PM2.5 reporting."""
         return "pm2_5" in self.raw["state"]
+
+    @property
+    def supports_air_quality(self) -> bool:
+        """Support Air quality reporting."""
+        return "airquality" in self.raw["state"]
