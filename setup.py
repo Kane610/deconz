c7134e681fafa746eee69f7295cb7ebb929916a1
Robert Svensson <Kane610@users.noreply.github.com>
2023-03-10 23:18:50 +0100

    Bump to v109

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.10"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-VERSION = "108"
+VERSION = "109"
 
 setup(
     name="pydeconz",
