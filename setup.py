593c89110a9e30bacf1bd3af94654435b37df26f
Robert Svensson <Kane610@users.noreply.github.com>
2023-11-20 20:36:29 +0100

    Bump to v114

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.11"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-VERSION = "113"
+VERSION = "114"
 
 setup(
     name="pydeconz",
