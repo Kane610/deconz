5789c9a783db9d72390f23baa041c1857a9248d4
MichaelHeimann <Michael@Heimann.ws>
2024-12-16 09:55:35 +0100

    remove trailing whitespace

--- pydeconz/models/sensor/presence.py
+++ pydeconz/models/sensor/presence.py
@@ -87,14 +87,14 @@ class PresenceConfigTriggerDistance(enum.StrEnum):
     NEAR = "near"
 
     # Trigger distance in meters
-    ONE = "1"                                                     
-    TWO = "2"                                                     
-    THREE = "3"                                                   
-    FOUR = "4"                           
-    FIVE = "5"                           
-    SIX = "6"                            
-    SEVEN = "7"                          
-    EIGHT = "8"                
+    ONE = "1"
+    TWO = "2"
+    THREE = "3"
+    FOUR = "4"
+    FIVE = "5"
+    SIX = "6"
+    SEVEN = "7"
+    EIGHT = "8"
     NINE = "9"
 
 
